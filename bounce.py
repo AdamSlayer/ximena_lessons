4565fcb3f345000738836b45e6e6a6466f58254e
AdamSlayer <76782559+AdamSlayer@users.noreply.github.com>
2021-01-13 14:44:08 +0100

    Add files via upload

--- bounce.py
+++ bounce.py
@@ -1,52 +1,49 @@
-from tkinter import*
+from tkinter import *
 import random
 import time
 
 
 class Ball:
     def __init__(self, canvas, paddle, color):
+
         self.canvas = canvas
         self.paddle = paddle
-        self.id = canvas.create_oval(10,10,25,25, fill=color)
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-        starts = [-3,-2,-1,1,2,3]
+        starts = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts)
-        self.x=starts[0]
-        self.y=3
-#        self.canvas_height = self.canvas.winfo_height()
-#        self.canvas_width = self.canvas.winfo_width()
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
 
+ 
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
-        if pos[1] <=0:
-            self.y=3
-        if pos[3] >=self.canvas_height:
-            self.y=-3
-
-#        if self.hit_paddle(pos) == True:
-#            self.y=-3
-
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.y = -3
+ #       if self.hit_paddle(pos) == True:
+  #          self.y = -3
         if pos[0] <= 0:
-            self.x=3
-        if pos[2] >=self.canvas_width:
-            self.x=-3
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+
 
 class Paddle:
-    def __init__(self, canvas, paddle, color):
+    def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
         self.canvas.move(self.id, 200, 300)
+        self.x = 0
         self.canvas_width = self.canvas.winfo_width()
-        self.canvas.bind_all("<KeyPress-Left>",self.turn_left)
-        self.canvas.bind_all("<KeyPress-Right>",self.turn_right)
-
-    def turn_left(self, evt):
-        self.x= -2
-
-    def turn_right(self, evt):
-        self.x= 2
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
 
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
@@ -56,16 +53,24 @@ class Paddle:
         elif pos[2] >= self.canvas_width:
             self.x = 0
 
+    def turn_left(self, evt):
+        self.x = -3
+
+    def turn_right(self, evt):
+        self.x = 3
+
+
 tk = Tk()
 tk.title("Game")
-tk.resizable(0,0)
+tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
-canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0, background="red")
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0, background='red')
 canvas.pack()
-tk.update
+tk.update()
 
-ball = Ball(canvas, paddle, "blue")
-paddle = Paddle(canvas, paddle, "yellow")
+paddle = Paddle(canvas, 'yellow')
+ball = Ball(canvas, paddle, 'blue')
+
 
 while 1:
     ball.draw()
@@ -73,5 +78,6 @@ while 1:
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+
 
 tk.mainloop()
