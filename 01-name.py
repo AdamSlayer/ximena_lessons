be9a7b2734663be6d45e4a3603ce710c73132e0b
AdamSlayer <76782559+AdamSlayer@users.noreply.github.com>
2021-01-13 14:38:32 +0100

    Add files via upload

---
+++ 01-name.py
@@ -0,0 +1,12 @@
+# -> Adam, here print your name
+
+# -> Simon Here print your name
+# Exercises:
+print ("Adam")
+print ("I dont like potatoes.")
+print ("I like dogs")
+print('Simon')
+print('Programing')
+print("I don't like mushrooms")
+
+print("Hola amigos")
---
+++ 03-variables.py
@@ -0,0 +1,14 @@
+#===========>Adam<===================
+name="Adam"
+age=13
+weight=60
+print("My name is:",name
+,weight
+,"Kg",age,"years")
+
+#===========>Simon<===================
+nameSimon = "Simon"
+ageSimon = 12
+weightSimon = 40
+print("My name is: ",nameSimon,"\n , I am: ",ageSimon," years old and I weight:",weightSimon,"\n kg, ", weightSimon * 2.205," lb")
+#
